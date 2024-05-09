d3f7266721096ff4bd4a087b39f48de898259ac2
Oliwia Rejman <o.rejman.418@studms.ug.edu.pl>
2024-05-09 14:21:43 +0200

    Spalanie lasu

--- Lab3/main.go
+++ Lab3/main.go
@@ -12,12 +12,17 @@ const (
 	burning
 )
 
-type forest [][]int
+type treeInfo struct {
+	state     int
+	humidity  int
+}
+
+type forest [][]treeInfo
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	width, height := 10, 10
+	width, height := 40, 10
 
 	f := makeForest(width, height)
 
@@ -38,7 +43,7 @@ func main() {
 func makeForest(width, height int) forest {
 	f := make(forest, height)
 	for i := range f {
-		f[i] = make([]int, width)
+		f[i] = make([]treeInfo, width)
 	}
 	return f
 }
@@ -47,33 +52,31 @@ func plantTrees(f forest) {
 	for y := range f {
 		for x := range f[y] {
 			if rand.Intn(2) == 0 {
-				f[y][x] = tree
+				f[y][x].state = tree
 			} else {
-				f[y][x] = empty 
+				f[y][x].state = empty
 			}
+			f[y][x].humidity = rand.Intn(101)
 		}
 	}
 }
 
-
 func burnForest(f forest) {
-
 	firstBurningX := rand.Intn(len(f[0]))
 	firstBurningY := rand.Intn(len(f))
-	f[firstBurningY][firstBurningX] = burning
-
+	f[firstBurningY][firstBurningX].state = burning
 
 	igniteNeighbors(f, firstBurningX, firstBurningY)
-	f[firstBurningY][firstBurningX] = empty
+	f[firstBurningY][firstBurningX].state = empty
 
 	for {
 		changed := false
 		for y := range f {
 			for x := range f[y] {
-				if f[y][x] == tree && isBurning(f, x, y) {
-					f[y][x] = burning
+				if f[y][x].state == tree && isBurning(f, x, y) {
+					f[y][x].state = burning
 					igniteNeighbors(f, x, y)
-					f[y][x] = empty
+					f[y][x].state = empty
 					changed = true
 				}
 			}
@@ -84,8 +87,6 @@ func burnForest(f forest) {
 	}
 }
 
-
-
 func isBurning(f forest, x, y int) bool {
 	for dy := -1; dy <= 1; dy++ {
 		for dx := -1; dx <= 1; dx++ {
@@ -93,7 +94,7 @@ func isBurning(f forest, x, y int) bool {
 				continue
 			}
 			ny, nx := y+dy, x+dx
-			if ny >= 0 && ny < len(f) && nx >= 0 && nx < len(f[ny]) && f[ny][nx] == burning {
+			if ny >= 0 && ny < len(f) && nx >= 0 && nx < len(f[ny]) && f[ny][nx].state == burning {
 				return true
 			}
 		}
@@ -101,27 +102,34 @@ func isBurning(f forest, x, y int) bool {
 	return false
 }
 
-
 func igniteNeighbors(f forest, x, y int) {
 	for dy := -1; dy <= 1; dy++ {
 		for dx := -1; dx <= 1; dx++ {
 			ny, nx := y+dy, x+dx
-			if ny >= 0 && ny < len(f) && nx >= 0 && nx < len(f[ny]) && (dx != 0 || dy != 0) && f[ny][nx] == tree {
-				f[ny][nx] = burning
+			if ny >= 0 && ny < len(f) && nx >= 0 && nx < len(f[ny]) && (dx != 0 || dy != 0) && f[ny][nx].state == tree {
+
+				if rand.Intn(101) <= f[ny][nx].humidity {
+					f[ny][nx].state = burning
+				}
 			}
 		}
 	}
 }
 
-
 func calculateBurnedPercent(f forest) float64 {
 	totalTrees := 0
+	for y := range f {
+		for x := range f[y] {
+			if f[y][x].state == tree || f[y][x].state == burning {
+				totalTrees++
+			}
+		}
+	}
+
 	burnedTrees := 0
 	for y := range f {
 		for x := range f[y] {
-			if f[y][x] == tree {
-				totalTrees++
-			} else if f[y][x] == burning {
+			if f[y][x].state == burning {
 				burnedTrees++
 			}
 		}
@@ -132,7 +140,7 @@ func calculateBurnedPercent(f forest) float64 {
 func printForest(f forest) {
 	for _, row := range f {
 		for _, cell := range row {
-			switch cell {
+			switch cell.state {
 			case empty:
 				fmt.Print(" ")
 			case tree:
